89732c3006affa62a3500c09ea3e03e7a9894101
Alexander Feldman <35717802+Feldman0007@users.noreply.github.com>
2019-09-25 22:47:52 -0700

    Update bot_v_bot.py

--- bot_v_bot.py
+++ bot_v_bot.py
@@ -4,22 +4,34 @@ from dlgo import gotypes
 from dlgo.utils import print_board, print_move
 import time
 
+
 # print("__name__ = ", __name__)
 
 def main():
-  board_size = 19
-  game = goboard.GameState.new_game(board_size)
-  bots = {
-    gotypes.Player.black: agent.naive.RandomBot(),
-    gotypes.Player.white: agent.naive.RandomBot(),
-  }
-  while not game.is_over():
-    time.sleep(0.3)
-    print(chr(27) + "[2J")
-    print_board(game.board)
-    bot_move = bots[game.next_player].select_move(game)
-    print_move(game.next_player, bot_move)
-    game = game.apply_move(bot_move)
+    while True:
+        board_size = input('Enter the dimensions of the square Go board. (Integer between 5 and 19):')
+        try:
+            board_size = int(board_size)
+        except ValueError:
+            print('Invalid Entry')
+            continue
+        if board_size in range(5, 20):
+            break
+        else:
+            print('Invalid Entry')
+    game = goboard.GameState.new_game(board_size)
+    bots = {
+        gotypes.Player.black: agent.naive.RandomBot(),
+        gotypes.Player.white: agent.naive.RandomBot(),
+    }
+    while not game.is_over():
+        time.sleep(0.3)
+        print(chr(27) + "[2J")
+        print_board(game.board)
+        bot_move = bots[game.next_player].select_move(game)
+        print_move(game.next_player, bot_move)
+        game = game.apply_move(bot_move)
+
 
 if __name__ == '__main__':
-  main()
+    main()
