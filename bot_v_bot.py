4bc2f43f17bfe18adb3f86fdff60126345292479
Natalie E. Ottolia <ottolianatalie@gmail.com>
2019-09-26 00:31:53 -0700

    set up script to compute score

--- bot_v_bot.py
+++ bot_v_bot.py
@@ -2,6 +2,7 @@ from dlgo import agent
 from dlgo import goboard
 from dlgo import gotypes
 from dlgo.utils import print_board, print_move
+from get_score import compute_score
 import time
 
 
@@ -27,6 +28,7 @@ def main():
     while not game.is_over():
         time.sleep(0.3)
         print(chr(27) + "[2J")
+        print ("WHITE: {} BLACK: {} ".format(compute_score("W"), compute_score("B")))
         print_board(game.board)
         bot_move = bots[game.next_player].select_move(game)
         print_move(game.next_player, bot_move)
---
+++ get_score.py
@@ -0,0 +1,5 @@
+def compute_score(player):
+    if player == "B":
+        return 5
+    elif player == "W":
+        return 10
