f6320506ad8e94692b4697eb83bff460a61b1140
MikeF0007 <31224282+MikeF0007@users.noreply.github.com>
2019-10-12 14:20:27 -0700

    Update mcts_go_cnn2.py

--- chapter_6_cnn/mcts_go_cnn2.py
+++ chapter_6_cnn/mcts_go_cnn2.py
@@ -4,8 +4,8 @@ from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Conv2D, MaxPooling2D
 
 np.random.seed(123)
-X = np.load('../generated_games/features.npy')
-Y = np.load('../generated_games/labels.npy')
+X = np.load('../generated_games/features-200.npy')
+Y = np.load('../generated_games/labels-200.npy')
 
 samples = X.shape[0]
 size = 9
